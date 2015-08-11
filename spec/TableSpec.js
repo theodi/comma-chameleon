c03a3d2d180716a8c0f2363b74ad3a022ddf380e
bcouston <ben.couston@theodi.org>
2015-08-11 14:41:08 +0100

    Tests for HandsonTable
    Not useful considering HandonTable already has its own Jasmine tests
    that test low level functionality. Keeping for future reference in case
    we move to a native functions for adding a row etc.

---
+++ spec/TableSpec.js
@@ -0,0 +1,20 @@
+// describe("table additions", function() {
+//
+//   var Fs = require('fs');
+//   var Ht = require('hands')
+//
+//   // Suggest two tests for open file - dialog test and, uploaded file to contents of table?
+//
+//   it("should be able to add a new column", function() {
+//     var hot = new Handsontable(container, {
+//       colHeaders: true,
+//       rowHeaders: true,
+//       columnSorting: true,
+//       contextMenu: true
+//     });
+//     columns_original = hot.countColumns()
+//     hot.add_column
+//     expect(hot.countColumns()).toEqual(columns_original + 1)
+//   });
+//
+// });
