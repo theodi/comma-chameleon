2809d7d2a0d60d0069a006ce9930e0bfa7f90bf3
pezholio <pezholio@gmail.com>
2015-08-12 12:29:47 +0100

    Fix title

--- main.js
+++ main.js
@@ -184,9 +184,9 @@ function createWindow(data, title) {
 
   // Open the devtools.
   mainWindow.openDevTools();
-  mainWindow.title = title;
 
   mainWindow.webContents.on('did-finish-load', function() {
+    mainWindow.setTitle(title);
     mainWindow.webContents.send('loadData', data);
   });
 
