3c6c57973d7e9fd1b3f78e564d790d83c495e20c
quadrophobiac <stephen.j.fortune@gmail.com>
2015-08-13 12:50:40 +0100

    a very down and dirty (DOM manipulation all the way down) employment of the tidy JSON viewer & editor, worth noting that I have taken the save function logic out of this script, need to check if this is the protocol for electron & IPC & renderer separation of concerns

--- editor.js
+++ editor.js
@@ -8,6 +8,28 @@ var ipc = require('ipc');
 
 var container = document.getElementById("jsoneditor");
 var editor = new JSONEditor(container);
+editor.setMode('text');
+
+var jsonSchema;
+
+document.getElementById("change-to-code-editor").addEventListener("click", function(){
+  console.log("you're pressing the code view");
+  editor.setMode('code');
+});
+
+document.getElementById("change-to-text-editor").addEventListener("click", function(){
+  editor.setMode('text');
+});
+
+document.getElementById("change-to-tree-view").addEventListener("click", function(){
+  editor.setMode('tree');
+});
+
+document.getElementById("save-schema").addEventListener("click", function(){
+  console.log("trigger for saving");
+  console.log(editor.get());
+  ipc.send('saveSchema', editor.get());
+});
 
 ipc.on('loadSchema', function(data){
   try {
@@ -15,9 +37,9 @@ ipc.on('loadSchema', function(data){
   } catch (jsonErr) {
     console.log(jsonErr);
   }
-
+  jsonSchema = json;
   editor.set(json);
+  document.getElementById("save-schema").className = 'btn btn-primary';
+  // workaround to set the button for saving to be anabled only when schema loaded
   var json = editor.get();
 });
-
-
