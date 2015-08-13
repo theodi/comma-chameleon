b39aadeae1417ccb70fbaf6a3b9175ca1ade81ce
pezholio <pezholio@gmail.com>
2015-08-13 09:05:34 +0100

    Formatting tweak

--- main.js
+++ main.js
@@ -243,32 +243,32 @@ function importExcel() {
   Dialog.showOpenDialog({ filters: [
     { name: 'text', extensions: ['xlsx', 'xls'] }
   ]}, function (fileNames) {
-      if (fileNames === undefined) return;
-      var fileName = fileNames[0];
-      var workbook = XLSX.readFile(fileName);
-      var first_sheet_name = workbook.SheetNames[0];
-      var worksheet = workbook.Sheets[first_sheet_name];
+    if (fileNames === undefined) return;
+    var fileName = fileNames[0];
+    var workbook = XLSX.readFile(fileName);
+    var first_sheet_name = workbook.SheetNames[0];
+    var worksheet = workbook.Sheets[first_sheet_name];
 
-      popup = new BrowserWindow({width: 300, height: 150, 'always-on-top': true});
-      popup.loadUrl('file://' + __dirname + '/select_worksheet.html');
+    popup = new BrowserWindow({width: 300, height: 150, 'always-on-top': true});
+    popup.loadUrl('file://' + __dirname + '/select_worksheet.html');
 
-      popup.webContents.on('did-finish-load', function() {
-        popup.webContents.send('loadSheets', workbook.SheetNames);
+    popup.webContents.on('did-finish-load', function() {
+      popup.webContents.send('loadSheets', workbook.SheetNames);
 
-        ipc.on('worksheetSelected', function(e, sheet_name) {
-          data = XLSX.utils.sheet_to_csv(workbook.Sheets[sheet_name]);
-          popup.close();
-          createWindow(data);
-        });
-
-        ipc.on('worksheetCanceled', function() {
-          popup.close();
-        });
+      ipc.on('worksheetSelected', function(e, sheet_name) {
+        data = XLSX.utils.sheet_to_csv(workbook.Sheets[sheet_name]);
+        popup.close();
+        createWindow(data);
       });
 
-      popup.on('closed', function() {
-        popup = null;
+      ipc.on('worksheetCanceled', function() {
+        popup.close();
       });
+    });
+
+    popup.on('closed', function() {
+      popup = null;
+    });
   });
 }
 
