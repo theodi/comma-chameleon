fadd6dbe5a421b790eb223e65a7018f4fcf4a896
quadrophobiac <stephen.j.fortune@gmail.com>
2015-09-15 15:26:32 +0100

    intent is to modularise the ipc calls into an index file for streamlined testing

---
+++ app/comma-chameleon/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by stephenfortune on 15/09/15.
+ */
+'require strict';
+
+//ipc.on('ragged_rows', function() {
+//  csv = hot.getData();
+//  fixRaggedRows(csv);
+//});
+//
+//ipc.on('schemaFromHeaders', function(){
+//  ipc.send('jsonHeaders',schemawizard.createSchema(returnHeaderRow()));
+//  schemawizard.createSchema(returnHeaderRow());
+//});
+//
+//ipc.on('validate', function() {
+//  validate();
+//});
+
