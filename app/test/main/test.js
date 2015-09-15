49465a0a77f4297ce583fbe23438e579766f3213
quadrophobiac <stephen.j.fortune@gmail.com>
2015-09-15 14:42:58 +0100

    rewriting this example test

--- app/test/main/test.js
+++ app/test/main/test.js
@@ -2,10 +2,7 @@ process.env.NODE_ENV = 'test' // this variable is set to permit access to the pa
 
 var assert = require('assert')
 var expect = require('expect.js')
-//import FileUtils from '../electron/file_utils';
-//import FileUtils from './file_utils';
-var FileUtils = require('./../../browser/file_utils')
-var datapackage = require('./../../browser/datapackage')
+
 /* global describe it */
 
 describe('describe: test 1', function () {
@@ -13,17 +10,3 @@ describe('describe: test 1', function () {
     assert(true)
   })
 })
-
-
-describe("testing an existing CSV edit object", function(){
-  //console.log(JSON.stringify(process.env));
-  //expect(datapackage.hello).to.be.a('function');
-  it('can successfully import an exported function and access private methods', function(){
-    expect(datapackage.exportDatapackage).to.be.a('function');
-    expect(datapackage._private.packageToJson).to.be.a('function');
-    expect(datapackage._private.zipPackage).to.be.a('function');
-  })
-
-  //expect(add).to.be.a('function');
-
-});
