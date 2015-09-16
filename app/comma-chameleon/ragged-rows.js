82e0738da57f2e0eb6b199995858de06a9d74847
quadrophobiac <stephen.j.fortune@gmail.com>
2015-09-16 12:45:43 +0100

    reviewing ragged rows has illustrated some flaws with the class (i) if there is an empty row in the CSV ragged row populates it with strings - this should be separate functionality (ii) the user prompt loop triggers even when the user access 'Fix Ragged Rows' from the menu, this shouldn't happen - suggest a tiny separate branch for fixing these issues

--- app/comma-chameleon/ragged-rows.js
+++ app/comma-chameleon/ragged-rows.js
@@ -3,28 +3,72 @@
   // Fills undefined cells with an empty string, keeping the table in a
   // rectangular format
 
-  var amendRows = function(csv_array) {
+//var detectRaggedRows = function(csv_array){
+//    var ragged = false;
+//    var cellContent = ""
+//  var column_length = getMaxColumns(csv_array);
+//  for (var y = 0; y < csv_array.length; y++) {
+//    for (var x = 0; x < column_length; x++) {
+//      cellContent = (hot.getDataAtCell(y,x);
+//      if (hot.getDataAtCell(y,x) === undefined) {
+//        var ragged = true;
+//        if (ragged_rows == 0) {
+//          userPrompt();
+//        }
+//        else if (ragged_rows == 1) {
+//          $messagePanel.append('<p>' + fixCell(csv_array,y,x) + '<p>')
+//        }
+//      }
+//    }
+//  }
+//}
+
+//var userPrompt function(){
+//  var amend = confirm("Your file has ragged rows, do you want to correct this?");
+//  var ragged_rows = 0;
+//  if(amend == true){
+//    ragged_rows = 1
+//  }
+//  if(ragged_rows == 1){
+//    fix();
+//    updateFeedback();
+//  }
+//}
+//
+//var fix = function(){
+//  csv_array[y].push("");
+//}
+//
+//var updateFeedback = function(){
+//  var $messagePanel = $('#message-panel');
+//  var logMsg = "Cell (" + String.fromCharCode(97 + x).toUpperCase() + "," + (y + 1) + ") has been added to file";
+//  $messagePanel.append('<p>' + logMsg + '<p>');
+//}
+
+  var amendRows = function(worksheet) {
     var $messagePanel = $('#message-panel');
     var ragged_rows = 0;
     //
-    for (var y = 0; y < csv_array.length; y++) {
-      for (var x = 0; x < getMaxColumns(csv_array); x++) {
+    for (var y = 0; y < worksheet.length; y++) {
+      for (var x = 0; x < getMaxColumns(worksheet); x++) {
         if (hot.getDataAtCell(y,x) === undefined) {
+          // only triggers if a cell returns undefined
           if (ragged_rows == 0) {
+            // this is a way of prompting once and then proceeding to fix every other ragged instance
             if (confirm("Your file has ragged rows, do you want to correct this?")) {
-              $('#right-panel').removeClass("hidden")
+              $('#right-panel').removeClass("hidden");
               ragged_rows = 1
-              $messagePanel.append('<p>' + fixCell(csv_array,y,x) + '<p>')
+              $messagePanel.append('<p>' + fixCell(worksheet,y,x) + '<p>')
             }
             else {ragged_rows = -1}
           }
           else if (ragged_rows == 1) {
-            $messagePanel.append('<p>' + fixCell(csv_array,y,x) + '<p>')
+            $messagePanel.append('<p>' + fixCell(worksheet,y,x) + '<p>')
           }
         }
       }
     }
-    updateTable(csv_array)
+    updateTable(worksheet)
   }
 
   function getMaxColumns(csv_array) {
@@ -40,7 +84,7 @@
 
   function fixCell(csv_array,y,x) {
     csv_array[y].push("")
-    var logMsg = "Cell (" + String.fromCharCode(97 + x).toUpperCase() + "," + (y + 1) + ") has been added to file"
+    var logMsg = "Cell (" + String.fromCharCode(97 + x).toUpperCase() + "," + (y + 1) + ") has been added to file";
     console.log(logMsg)
     return logMsg
   }
--- app/test/renderer/ragged-rows_spec.js
+++ app/test/renderer/ragged-rows_spec.js
@@ -8,7 +8,31 @@ var expect = require('expect.js')
 
 var rows = require('./../../comma-chameleon/ragged-rows')
 
-describe('testing ragged row functions', function(){
+describe('testing ragged row functions against 2D array', function(){
+
+  it('checks a loaded CSV and returns prompt on first discovery of ragged row', function(){
+
+  });
+
+  it('if ragged rows present and user consent it parses the entire CSV', function(){
+
+  })
+
+  it('well formed array results in no DOM change', function(){
+
+    tableState = '';
+    // pass a non ragged array
+    // tableState should be unchanged
+
+  });
+
+  it('at least one ragged array triggers a prompt', function(){
+    // expect prompt
+    // expect change in hidden status
+
+  });
+
+  it('ragged array and yes to prompt equates to change on HandsOnTable object')
 
 
 })
