a188d609fecef2787f1ebad0aa2b321c08e8c1f6
quadrophobiac <stephen.j.fortune@gmail.com>
2015-09-16 14:16:00 +0100

    this test file includes two means of accessing the Handsontable library which is installed via Bower - the latter test is preferable as it follows the module loading being adhered to within the application

--- app/test/renderer/hand-on-table_spec.js
+++ app/test/renderer/hand-on-table_spec.js
@@ -3,14 +3,13 @@
  */
 var expect = require('chai').expect;
 var should = require('chai').should();
-//var hotController = require('./../../comma-chameleon/hot.js'); // failing probably cause it cant access the bower_components
+var hotController = require('./../../comma-chameleon/hot.js'); // failing probably cause it cant access the bower_components
 var handMadeHOT = require('./../../../bower_components/handsontable/dist/handsontable.full.js');
 
 describe('loading Hands On Table library into workview', function(){
 
-  it('constructs a Hands On Table element', function(){
+  it('constructs a Hands On Table element from source', function(){
     var hotView = document.createElement('div');
-    console.log(handMadeHOT.toString());
     var data = [
       ["", "Ford", "Volvo", "Toyota", "Honda"],
       ["2014", 10, 11, 12, 13],
@@ -25,12 +24,19 @@ describe('loading Hands On Table library into workview', function(){
       columnSorting: true,
       contextMenu: false
     });
-    console.log(hot.toString());
-    console.log(Object.getOwnPropertyNames(hot));
-    //var worksheet = hot.loadData(data);
-    console.log(hot.getData());
-    console.log(JSON.stringify(hot.data));
-    //hotController.create(hotView);
+    expect(hot.getData()).to.equal(data);
+  });
+
+  it('constructs hands on table from programs source files', function(){
+    var hotView = document.createElement('div');
+    var data = [
+      ["", "Ford", "Volvo", "Toyota", "Honda"],
+      ["2014", 10, 11, 12, 13],
+      ["2015", 20, 11, 14, 13],
+      ["2016", 30, 15, 12, 13]
+    ];
+    var hot = hotController.create(hotView);
+    hot.loadData(data);
     expect(hot.getData()).to.equal(data);
   });
 
