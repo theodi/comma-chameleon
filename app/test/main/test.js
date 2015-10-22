b3db491c87aa99d4e9c5326c3ed1e22e372a663f
quadrophobiac <stephen.j.fortune@gmail.com>
2015-10-22 15:19:56 +0100

    redundant require removed   BDD and TDD assertions now managed by chai

--- app/test/main/test.js
+++ app/test/main/test.js
@@ -1,7 +1,6 @@
 process.env.NODE_ENV = 'test' // this variable is set to permit access to the packages private methods
 
-var assert = require('assert')
-var expect = require('expect.js')
+var assert = require('chai').assert;
 
 /* global describe it */
 
