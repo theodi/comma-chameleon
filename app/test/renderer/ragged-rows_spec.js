f7ae7fcb485abd884106b4cb99c9b1c0a948b504
quadrophobiac <stephen.j.fortune@gmail.com>
2015-10-26 19:23:24 +0000

    including hooks in related spec ragged rows will also use the same establishing hooks as hands-on-table specs

--- app/test/renderer/ragged-rows_spec.js
+++ app/test/renderer/ragged-rows_spec.js
@@ -7,11 +7,31 @@ var assert = require('chai').assert
 var expect = require('chai').expect
 var should = require('chai').should();
 
-var rows = require('./../../comma-chameleon/ragged-rows')
+var hotController = require('./../../comma-chameleon/hot.js');
+
+
+beforeEach(function () {
+  hotView = document.createElement('div');
+  hot = hotController.create(hotView);
+});
+
 
 describe('testing ragged row functions against 2D array', function(){
 
   it('checks a loaded CSV and returns prompt on first discovery of ragged row', function(){
+
+    var panel = document.createElement('div');
+    panel.id = "#right-panel";
+
+    var data = [
+      ["", "Ford", "Volvo", "Toyota", "Honda"],
+      ["2014", 10, 11, 12, 13],
+      ["2015", 20, 11, 14],
+      ["2016", 30, 15, 12, 13]
+    ];
+
+    hot.loadData(data);
+    //raggedRows.fixRaggedRows(hot.getData());
 
   });
 
