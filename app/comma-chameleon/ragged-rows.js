3b29522a27a58bdcb0a569bbd53bf38223474e58
quadrophobiac <stephen.j.fortune@gmail.com>
2015-10-26 16:39:14 +0000

    removing commented out cruft the preceding code has been changed due to following a module exports pattern

--- app/comma-chameleon/ragged-rows.js
+++ app/comma-chameleon/ragged-rows.js
@@ -3,48 +3,6 @@
   // Fills undefined cells with an empty string, keeping the table in a
   // rectangular format
 
-//var detectRaggedRows = function(csv_array){
-//    var ragged = false;
-//    var cellContent = ""
-//  var column_length = getMaxColumns(csv_array);
-//  for (var y = 0; y < csv_array.length; y++) {
-//    for (var x = 0; x < column_length; x++) {
-//      cellContent = (hot.getDataAtCell(y,x);
-//      if (hot.getDataAtCell(y,x) === undefined) {
-//        var ragged = true;
-//        if (ragged_rows == 0) {
-//          userPrompt();
-//        }
-//        else if (ragged_rows == 1) {
-//          $messagePanel.append('<p>' + fixCell(csv_array,y,x) + '<p>')
-//        }
-//      }
-//    }
-//  }
-//}
-
-//var userPrompt function(){
-//  var amend = confirm("Your file has ragged rows, do you want to correct this?");
-//  var ragged_rows = 0;
-//  if(amend == true){
-//    ragged_rows = 1
-//  }
-//  if(ragged_rows == 1){
-//    fix();
-//    updateFeedback();
-//  }
-//}
-//
-//var fix = function(){
-//  csv_array[y].push("");
-//}
-//
-//var updateFeedback = function(){
-//  var $messagePanel = $('#message-panel');
-//  var logMsg = "Cell (" + String.fromCharCode(97 + x).toUpperCase() + "," + (y + 1) + ") has been added to file";
-//  $messagePanel.append('<p>' + logMsg + '<p>');
-//}
-
   var amendRows = function(worksheet) {
     var $messagePanel = $('#message-panel');
     var ragged_rows = 0;
