8a1b39eebf501cf90b9aa8ad505905b2e745c8bf
quadrophobiac <stephen.j.fortune@gmail.com>
2015-10-27 17:28:25 +0000

    spec with indicative testing assertions ragged-rows-spec has been updated with several TODOs which cannot be rectified until a refactor of ragged-rows and its model dialog occurs see (ccdae93)[https://github.com/theodi/comma-chameleon/commit/ccdae93f631e2897894d04998b484c964133d548] for more on this necessity

--- app/test/renderer/ragged-rows_spec.js
+++ app/test/renderer/ragged-rows_spec.js
@@ -22,11 +22,27 @@ before(function(){
 
 beforeEach(function () {
   hotView = document.createElement('div');
+  //document.body.appendChild(hotView);
   hot = hotController.create(hotView);
 });
 
 
 describe('testing ragged row functions against 2D array', function(){
+
+  it('well formed array results in no DOM change', function(){
+
+    var data = [
+      ["", "Ford", "Volvo", "Toyota", "Honda"],
+      ["2014", 10, 11, 12, 13],
+      ["2015", 20, 11, 14, 11],
+      ["2016", 30, 15, 12, 13]
+    ];
+
+    hot.loadData(data);
+    raggedRows.fixRaggedRows(hot.getData());
+    expect(mpanel.innerText).to.not.have.string('has been added to file');
+
+  });
 
   it('checks a loaded CSV and returns prompt on first discovery of ragged row', function(){
 
@@ -39,29 +55,43 @@ describe('testing ragged row functions against 2D array', function(){
 
     hot.loadData(data);
     raggedRows.fixRaggedRows(hot.getData());
-    expect(mpanel.innerText).to.have.string('has been added to file');
-
-  });
-
-  it('if ragged rows present and user consent it parses the entire CSV', function(){
-
-  })
-
-  it('well formed array results in no DOM change', function(){
-
-    tableState = '';
-    // pass a non ragged array
-    // tableState should be unchanged
-
-  });
-
-  it('at least one ragged array triggers a prompt', function(){
-    // expect prompt
-    // expect change in hidden status
+    //TODO code that automates the answer of yes to modal dialog
+    //TODO a spy that confirms modal dialog closed
+    //expect(mpanel.innerText).to.have.string('has been added to file'); TODO uncomment once the above refactor done
 
   });
 
   it('ragged array and yes to prompt equates to change on HandsOnTable object', function(){
+    var data = [
+      ["", "Ford", "Volvo", "Toyota", "Honda"],
+      ["2014", 10, 11, 12, 13],
+      ["2015", 20, 11, 14],
+      ["2016", 30, 15, 12, 13]
+    ];
+
+    hot.loadData(data);
+    raggedRows.fixRaggedRows(hot.getData());
+    //TODO code that automates the answer of yes to modal dialog
+    //expect(mpanel.innerText).to.have.string('has been added to file'); TODO uncomment once the above refactor done
+  });
+
+
+  it('if ragged rows present and user consent it parses the entire CSV', function(){
+
+    var data = [
+      ["", "Ford", "Volvo", "Toyota", "Honda"],
+      ["2014", 10, 11, 12, 13],
+      ["2015", 20, 11, 14],
+      ["2016", 30, 15, 12, 13],
+      ["2014", 10, 11, 12, 13],
+      ["2015", 20, 11, 14],
+      ["2016", 30, 15, 12, 13]
+    ];
+
+    hot.loadData(data);
+    raggedRows.fixRaggedRows(hot.getData());
+    //expect(mpanel.innerText).to.have.string('has been added to file');
+    //TODO - an assertion that determines that the above should have been added twice
 
   });
 
