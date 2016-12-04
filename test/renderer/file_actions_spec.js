29d51063af36561a1c617340f9469ddb15212776
chris48s <c.mshaw@yahoo.co.uk>
2016-12-04 11:49:56 +0000

    Fix error in unit test caused by global scope of var 'data'
    This test was incorrectly passing because the var 'data' was:
    - defined in the global scope
    - assigned in the test
    - re-assigned in the code called by the test
    - used in an assertion
    
    Adding missing var statements revealed this.

--- test/renderer/file_actions_spec.js
+++ test/renderer/file_actions_spec.js
@@ -1,9 +1,9 @@
-process.env.NODE_ENV = 'test'
+process.env.NODE_ENV = 'test';
 
-var chai = require('chai')
+var chai = require('chai');
 
-var assert = chai.assert
-var expect = chai.expect
+var assert = chai.assert;
+var expect = chai.expect;
 var should = chai.should();
 
 var $ = require('./../../bower_components/jquery/dist/jquery.js');
@@ -21,25 +21,25 @@ beforeEach(function () {
 describe('open file', function() {
 
   it('opens a file', function() {
-    data = "foo,bar,baz\r\n1,2,3\r\n4,5,6"
-    file_actions.open(hot, data)
-    expect(hot.getData()).to.eql([['foo', 'bar', 'baz'],['1','2','3'],['4','5','6']])
-  })
+    var data = "foo,bar,baz\r\n1,2,3\r\n4,5,6";
+    file_actions.open(hot, data);
+    expect(hot.getData()).to.eql([['foo', 'bar', 'baz'],['1','2','3'],['4','5','6']]);
+  });
 
-})
+});
 
 describe('save file', function() {
 
   it('saves a file', function() {
-    data = "foo,bar,baz\r\n1,2,3\r\n4,5,6"
-    file_actions.open(hot, data)
-    file_actions.save(hot, '/tmp/mycsv.csv')
+    var data = "foo,bar,baz\r\n1,2,3\r\n4,5,6\r\n";
+    file_actions.open(hot, data);
+    file_actions.save(hot, '/tmp/mycsv.csv');
 
     fs.readFile('/tmp/mycsv.csv', 'utf-8', function (err, d) {
-      expect(d).to.eq(data)
-    })
+      expect(d).to.eq(data);
+    });
 
-    expect(document.title).to.eq('/tmp/mycsv.csv')
-  })
+    expect(document.title).to.eq('/tmp/mycsv.csv');
+  });
 
-})
+});
