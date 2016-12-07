b56a34d7424a6b41fc92654ef16359e3293d10ca
chris48s <chris48s@users.noreply.github.com>
2016-12-07 09:16:07 +0000

    add ability to remove a range of rows or columns (#124)

--- renderer/menu.js
+++ renderer/menu.js
@@ -36,20 +36,40 @@ var columnRight = new MenuItem({
 })
 
 var removeRow = new MenuItem({
-  label: 'Remove entire row',
+  label: 'Remove row(s)',
   click: function() {
-    hot.alter('remove_row', hot.getSelected()[0])
-    hot.deselectCell()
+    var range = hot.getSelectedRange();
+
+    var start = Math.min(range.from.row, range.to.row);
+    var end   = Math.max(range.from.row, range.to.row);
+
+    for (var row = start; row <= end; row++) {
+      // rows are re-indexed after each remove
+      // so always remove 'start'
+      hot.alter('remove_row', start);
+    }
+
+    hot.deselectCell();
   }
-})
+});
 
 var removeCol = new MenuItem({
-  label: 'Remove entire column',
+  label: 'Remove column(s)',
   click: function() {
-    hot.alter('remove_col', hot.getSelected()[1])
-    hot.deselectCell()
+    var range = hot.getSelectedRange();
+
+    var start = Math.min(range.from.col, range.to.col);
+    var end   = Math.max(range.from.col, range.to.col);
+
+    for (var col = start; col <= end; col++) {
+      // cols are re-indexed after each remove
+      // so always remove 'start'
+      hot.alter('remove_col', start);
+    }
+
+    hot.deselectCell();
   }
-})
+});
 
 menu.append(rowAbove);
 menu.append(rowBelow);
