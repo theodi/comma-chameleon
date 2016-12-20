0dc98568304a050a4e32404263a1d90871c17218
chris48s <c.mshaw@yahoo.co.uk>
2016-12-20 19:39:49 +0000

    Modify file tests to pass assertion in callback
      Prevents race condition where we are trying to
      make read from file before it is written

--- test/renderer/file_actions_spec.js
+++ test/renderer/file_actions_spec.js
@@ -44,14 +44,13 @@ describe('save file', function() {
   it('saves a file', function(done) {
     var data = "foo,bar,baz\r\n1,2,3\r\n4,5,6\r\n";
     file_actions.open(hot, data);
-    file_actions.save(hot, os.tmpdir() + '/mycsv.csv');
-
-    fs.readFile(os.tmpdir() + '/mycsv.csv', 'utf-8', function (err, d) {
-      expect(d).to.eq(data);
-      expect(document.title).to.eq(os.tmpdir() + '/mycsv.csv');
-      done()
+    file_actions.save(hot, os.tmpdir() + '/mycsv.csv', file_actions.formats.csv, function() {
+      fs.readFile(os.tmpdir() + '/mycsv.csv', 'utf-8', function (err, d) {
+        expect(d).to.eq(data);
+        expect(document.title).to.eq(os.tmpdir() + '/mycsv.csv');
+        done();
+      });
     });
-
   });
 
 });
@@ -61,11 +60,11 @@ describe('convert file', function() {
   it('converts a file from csv to tsv', function(done) {
     var data = "foo,bar,baz\r\n1,2,3\r\n4,5,6";
     file_actions.open(hot, data);
-    file_actions.save(hot, os.tmpdir() + '/mytsv.tsv', file_actions.formats.tsv);
-
-    fs.readFile(os.tmpdir() + '/mytsv.tsv', 'utf-8', function (err, d) {
-      expect(d).to.eq("foo\tbar\tbaz\r\n1\t2\t3\r\n4\t5\t6\r\n");
-      done()
+    file_actions.save(hot, os.tmpdir() + '/mytsv.tsv', file_actions.formats.tsv, function() {
+      fs.readFile(os.tmpdir() + '/mytsv.tsv', 'utf-8', function (err, d) {
+        expect(d).to.eq("foo\tbar\tbaz\r\n1\t2\t3\r\n4\t5\t6\r\n");
+        done();
+      });
     });
   });
 
