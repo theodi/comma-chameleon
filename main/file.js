cbdc4bea2071035aab454b3d5ff98f24864c91ef
chris48s <chris48s@users.noreply.github.com>
2017-02-28 09:15:52 +0000

    add support for custom formats (#139)
    * add support for custom formats
    
    * close dialog in 'formatSelected' handler
    
        ..and remove stray console.log

--- main/file.js
+++ main/file.js
@@ -1,3 +1,14 @@
+function makeCustomFormat(separator, delimiter) {
+  // assemble a format object describing a custom format
+  return {
+    label: 'Custom',
+    filters: [],
+    options: { separator: separator, delimiter: delimiter},
+    mime_type: 'text/plain',
+    default_extension: 'txt',
+  };
+}
+
 function openFile(format) {
   Dialog.showOpenDialog({
       filters: format.filters
@@ -8,14 +19,46 @@ function openFile(format) {
   );
 }
 
-function saveFileAs(format) {
+function openCustom() {
   var window = BrowserWindow.getFocusedWindow();
+  var dialog = new BrowserWindow({width: 200, height: 400});
+  dialog.once('closed', function() {
+    ipc.removeAllListeners('formatSelected');
+    dialog = null;
+  });
+  ipc.once('formatSelected', function(event, data) {
+    dialog.close();
+    var format = makeCustomFormat(data.separator, data.delimiter);
+    openFile(format);
+  });
+  dialog.loadURL('file://' + __dirname + '/../views/custom_format.html');
+}
+
+function saveFileAs(format, window) {
+  if (!window) {
+    window = BrowserWindow.getFocusedWindow();
+  }
   Dialog.showSaveDialog({ filters: format.filters }, function (fileName) {
     if (fileName === undefined) return;
     window.webContents.send('saveData', fileName, format);
     utils.enableSave();
     window.format = format;
   });
+}
+
+function saveAsCustom() {
+  var window = BrowserWindow.getFocusedWindow();
+  var dialog = new BrowserWindow({width: 200, height: 400});
+  dialog.once('closed', function() {
+    ipc.removeAllListeners('formatSelected');
+    dialog = null;
+  });
+  ipc.once('formatSelected', function(event, data) {
+    dialog.close();
+    var format = makeCustomFormat(data.separator, data.delimiter);
+    saveFileAs(format, window);
+  });
+  dialog.loadURL('file://' + __dirname + '/../views/custom_format.html');
 }
 
 function saveFile() {
@@ -38,7 +81,9 @@ function readFile(fileNames, format) {
 
 module.exports = {
   openFile,
+  openCustom,
   readFile,
   saveFileAs,
+  saveAsCustom,
   saveFile
 };
