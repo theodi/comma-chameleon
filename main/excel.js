31a4335a049faa33bd615eb2508d28de253a4104
Stephen Fortune <stephen.fortune@theodi.org>
2017-07-13 15:56:59 +0100

    Casing and variable assignment cleaning

--- main/excel.js
+++ main/excel.js
@@ -5,16 +5,16 @@ function importExcel () {
     if (fileNames === undefined) return
     var fileName = fileNames[0]
     var workbook = XLSX.readFile(fileName)
-    var first_sheet_name = workbook.SheetNames[0]
-    var worksheet = workbook.Sheets[first_sheet_name]
+    var FirstSheetName = workbook.SheetNames[0]
+    var worksheet = workbook.Sheets[FirstSheetName]
 
-    popup = new BrowserWindow({width: 300, height: 150})
+    var popup = new BrowserWindow({width: 300, height: 150})
     popup.loadURL('file://' + __dirname + '/../views/select_worksheet.html')
     popup.webContents.on('did-finish-load', function () {
       popup.webContents.send('loadSheets', workbook.SheetNames)
 
-      ipc.once('worksheetSelected', function (e, sheet_name) {
-        data = XLSX.utils.sheet_to_csv(workbook.Sheets[sheet_name])
+      ipc.once('worksheetSelected', function (e, SheetName) {
+        var data = XLSX.utils.sheet_to_csv(workbook.Sheets[SheetName])
         popup.close()
         utils.createWindow(data)
       })
